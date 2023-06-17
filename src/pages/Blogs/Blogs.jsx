398bcd35be7e77bffa64415f8f8d389cdac367b5
kapilrohilla <kapilrohilla2002@gmail.com>
2023-06-17 15:00:23 +0530

    blog section completed

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -2,8 +2,77 @@ import React from 'react';
 import './Blogs.scss';
 const Blogs = () => {
     return (
-        <div>
-            Blogs
+        <div className="blogs">
+            <section className='most_recent_post_section'>
+                <div className="secNav">
+                    <h2>Customer Service</h2>
+                    <div className="right">
+                        <span>view more &gt;</span>
+                    </div>
+                </div>
+                <div className="most_recents_post">
+                    <div className="left">
+                        <div className="most_recent_blog">
+                            <img src="https://hs-marketing-contentful.imgix.net/https%3A%2F%2Fimages.ctfassets.net%2Fp15sglj92v6o%2F2yk11CW1XDSpNUgrtE0JbK%2F8b36fa60bf2ae550b6c76f75c20c1f49%2FBlog_Illo.png?ixlib=gatsbySourceUrl-2.1.1&auto=format%2C%20compress&w=814&h=458&s=9a910ebdc78ff0e97de3fed9f7c24e05" alt="" />
+                            <div className="detail">
+                                <small>customer service</small>
+                                <h2>Building a Customer-Centric Ecommerce Business: The Complete Guide</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <div className="most_recent_blog">
+                            <img src="https://hs-marketing-contentful.imgix.net/https%3A%2F%2Fimages.ctfassets.net%2Fp15sglj92v6o%2FNfg3eS6xw6SCCfRIjOX8E%2F693766c7ad69daf5d13fceb4a83d7b0f%2F8_Best_Customer_Service_Mobile_Apps__Features___Pricing_.png?ixlib=gatsbySourceUrl-2.1.1&auto=format%2C%20compress&w=814&h=457&s=98a23b1335199ef3056e49f7feff41c2" alt="" />
+                            <div className="detail">
+                                <small>customer service</small>
+                                <h2>The 8 Best Customer Service Mobile Apps (Features + Pricing)</h2>
+                            </div>
+                        </div>
+
+                        <div className="most_recent_blog">
+                            <img src="https://hs-marketing-contentful.imgix.net/https%3A%2F%2Fimages.ctfassets.net%2Fp15sglj92v6o%2F2NDeW5owbC4g6iYWeR63hI%2F64da7d34f4500dfd766fce68234cbf9a%2FBlog_Template2x.png?ixlib=gatsbySourceUrl-2.1.1&auto=format%2C%20compress&w=814&h=456&s=d42bdc0c949d4ac0a0cf313c132884c2" alt="" />
+                            <div className="detail">
+                                <small>customer service</small>
+                                <h2>Attract, Delight, Repeat: How To Turn Ecommerce Buyers Into Repeat Customers</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <h2>Discover the latest in..</h2>
+            <section className="customer_service">
+                <div className="secNav">
+                    <h2>Customer Service</h2>
+                    <div className="right">
+                        <span>view more &gt;</span>
+                    </div>
+                </div>
+                <div className="customer_service_blog_container">
+                    <div className="customer_service_blog">
+                        <img src="https://hs-marketing-contentful.imgix.net/https%3A%2F%2Fimages.ctfassets.net%2Fp15sglj92v6o%2F2yk11CW1XDSpNUgrtE0JbK%2F8b36fa60bf2ae550b6c76f75c20c1f49%2FBlog_Illo.png?ixlib=gatsbySourceUrl-2.1.1&auto=format%2C%20compress&w=814&h=458&s=9a910ebdc78ff0e97de3fed9f7c24e05" alt="" />
+                        <div className="detail">
+                            <small>customer service</small>
+                            <h2>Building a Customer-Centric Ecommerce Business: The Complete Guide</h2>
+                        </div>
+                    </div>
+
+                    <div className="customer_service_blog">
+                        <img src="https://hs-marketing-contentful.imgix.net/https%3A%2F%2Fimages.ctfassets.net%2Fp15sglj92v6o%2FNfg3eS6xw6SCCfRIjOX8E%2F693766c7ad69daf5d13fceb4a83d7b0f%2F8_Best_Customer_Service_Mobile_Apps__Features___Pricing_.png?ixlib=gatsbySourceUrl-2.1.1&auto=format%2C%20compress&w=814&h=457&s=98a23b1335199ef3056e49f7feff41c2" alt="" />
+                        <div className="detail">
+                            <small>customer service</small>
+                            <h2>The 8 Best Customer Service Mobile Apps (Features + Pricing)</h2>
+                        </div>
+                    </div>
+
+                    <div className="customer_service_blog">
+                        <img src="https://hs-marketing-contentful.imgix.net/https%3A%2F%2Fimages.ctfassets.net%2Fp15sglj92v6o%2F2NDeW5owbC4g6iYWeR63hI%2F64da7d34f4500dfd766fce68234cbf9a%2FBlog_Template2x.png?ixlib=gatsbySourceUrl-2.1.1&auto=format%2C%20compress&w=814&h=456&s=d42bdc0c949d4ac0a0cf313c132884c2" alt="" />
+                        <div className="detail">
+                            <small>customer service</small>
+                            <h2>Attract, Delight, Repeat: How To Turn Ecommerce Buyers Into Repeat Customers</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
