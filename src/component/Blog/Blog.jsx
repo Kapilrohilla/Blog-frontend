77053218ee122b69e6f7a547584735f60a797fd5
kapilrohilla <kapilrohilla2002@gmail.com>
2023-06-17 07:47:09 +0530

    Fixed grid issue at homepage

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Blog.scss';
+import { Link } from 'react-router-dom';
 const Blog = ({ d }) => {
     return (
-        <div className="blog">
-            <div className="left">
-                <img src={d.img ? d.img : "/image/Image_not_available.png"} alt="" />
+        <Link to='' className='blogLink'>
+            <div className="blog">
+                <div className="left">
+                    <img src={d.img ? d.img : "/image/Image_not_available.png"} alt="" />
+                </div>
+                <div className="right">
+                    <p className="tag">{d.tag ? d.tag : "uncategorized"}</p>
+                    <h3>{d.title}</h3>
+                    <p className="para">{d.body}</p>
+                </div>
             </div>
-            <div className="right">
-                <p className="tag">{d.tag ? d.tag : "uncategorized"}</p>
-                <h3>{d.title}</h3>
-                <p className="para">{d.body}</p>
-            </div>
-        </div>
+        </Link>
     )
 }
 
